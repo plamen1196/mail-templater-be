1e8904245b33e22f5a26bec53208eddb3f8ebacb
tacho <tachojelev@gmail.com>
2022-05-04 18:58:28 +0300

    Add dev and prod profiles

--- src/test/java/com/fmi/mailtemplaterbe/MailTemplaterBeApplicationTests.java
+++ src/test/java/com/fmi/mailtemplaterbe/MailTemplaterBeApplicationTests.java
@@ -1,13 +1,10 @@
 package com.fmi.mailtemplaterbe;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MailTemplaterBeApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void test() {
 	}
-
 }
