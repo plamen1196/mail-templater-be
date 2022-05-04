64584bb2e599b75977f5292ed993fc9fb8be9ddc
tacho <tachojelev@gmail.com>
2022-05-04 20:32:23 +0300

    Add createSMTPSession method to SmtpService

--- src/main/java/com/fmi/mailtemplaterbe/service/SmtpService.java
+++ src/main/java/com/fmi/mailtemplaterbe/service/SmtpService.java
@@ -2,9 +2,13 @@ package com.fmi.mailtemplaterbe.service;
 
 import com.fmi.mailtemplaterbe.config.SmtpConfiguration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
 import java.util.List;
+import java.util.Properties;
 
 @Service
 @RequiredArgsConstructor
@@ -12,11 +16,34 @@ public class SmtpService {
 
     private final SmtpConfiguration smtpConfiguration;
 
-    public List<SmtpConfiguration.SmtpServer> getSmtpServers() {
-        return smtpConfiguration.getServers();
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
+
+    public Session createSMTPSession() {
+        final SmtpConfiguration.SmtpServer defaultSmtpServer = getDefaultSmtpServer();
+        final Properties prop = new Properties();
+
+        prop.put("mail.smtp.host", defaultSmtpServer.getHost());
+        prop.put("mail.smtp.port", defaultSmtpServer.getPort());
+        prop.put("mail.smtp.auth", defaultSmtpServer.getAuth());
+        prop.put("mail.smtp.starttls.enable", defaultSmtpServer.getStarttls().getEnable()); // TLS
+        prop.put("mail.smtp.ssl.trust", defaultSmtpServer.getHost());
+        prop.put("mail.smtp.timeout", defaultSmtpServer.getTimeout());
+        prop.put("mail.smtp.connectiontimeout", defaultSmtpServer.getConnectiontimeout());
+
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+        return session;
     }
 
-    public SmtpConfiguration.SmtpServer getDefaultSmtpServer() {
+    private SmtpConfiguration.SmtpServer getDefaultSmtpServer() {
         return smtpConfiguration.getServers().stream()
                 .filter(smtpServer -> smtpServer.getName().equalsIgnoreCase(smtpConfiguration.getDefaultServerName()))
                 .findFirst()
